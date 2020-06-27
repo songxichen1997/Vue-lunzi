861c1f7c5f2da4e6bf5b91ab585e8ed45279e1a6
songxichen1997 <752334578@qq.com>
2020-06-28 01:05:27 +0800

    将svg代码整理到icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href=`#i-${name}`></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: "icon",
+    props: {
+      name: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .g-icon {
+    width: 1em;
+    height: 1em;
+  }
+</style>
