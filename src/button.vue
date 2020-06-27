c466315e82484203eda414a1fb4029d8b8a626a7
songxichen1997 <752334578@qq.com>
2020-06-28 00:42:14 +0800

    添加icon 与 icon属性

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,65 @@
 <template>
-    <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-    export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 
 <style lang="scss">
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-        &:active {
-            background: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
-        }
+  .g-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    > .content {
+      order: 2;
     }
+
+    > .icon {
+      order: 1;
+      vertical-align: middle;
+      margin-right: .1em;
+    }
+
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+
+    &:active {
+      background: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+
+      > .icon {
+        margin-left: .1em;
+        margin-right: 0;
+        order: 2;
+      }
+
+    }
+  }
 </style>
